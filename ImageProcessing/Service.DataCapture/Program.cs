a9b2c08606072d41a10fa4dda4dea6e66fcc4aee
Ozan Bayram <ozan_bayram@epam.com>
2022-12-08 00:26:21 +0300

    Simple publish & consume

--- ImageProcessing/Service.DataCapture/Program.cs
+++ ImageProcessing/Service.DataCapture/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+
+Console.WriteLine("Started: Data Capture Service.");
+
+const string connectionUri = "amqp://guest:guest@localhost:5672";
+const string exchangeName = "Exchange.DataCaptureService";
+const string routingKey = "";
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri(connectionUri);
+
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+
+const string infoMessage = "Please, enter your message. 'Q' to stop sending.";
+Console.WriteLine(infoMessage);
+var input = string.Empty;
+
+while (input != "Q" && input != "q")
+{
+    input = Console.ReadLine();
+
+    if (input == "Q" || input == "q")
+    {
+        break;
+    }
+    else 
+    {
+        if (!string.IsNullOrEmpty(input))
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+            channel.BasicPublish(exchangeName, routingKey, null, bytes);
+        }
+        else
+        {
+            Console.WriteLine(infoMessage);
+        }
+    } 
+}
+
+Console.WriteLine("Terminating: Data Capture Service.");
+
+channel.Close();
+connection.Close();
--- ImageProcessing/Service.MainProcessing/Program.cs
+++ ImageProcessing/Service.MainProcessing/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Started: Main Processing.");
+
+const string connectionUri = "amqp://guest:guest@localhost:5672";
+const string exchangeName = "Exchange.DataCaptureService";
+const string routingKey = "";
+const string queueName = "Queue.DataCaptureService";
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri(connectionUri);
+
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.ExchangeDeclare(exchangeName
+    , ExchangeType.Fanout
+    , true);
+
+channel.QueueDeclare(queueName, true, false, false);
+channel.QueueBind(queueName, exchangeName, routingKey);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, eventArgs) => 
+{
+    var message = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume(queueName, true, consumer);
+
+Console.WriteLine("'Enter' to stop receiving.");
+Console.ReadLine();
+
+channel.Close();
+connection.Close();
